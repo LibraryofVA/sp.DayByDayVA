eb3a83a33f41a32a57bf8c65a3e7e692f1d5d739
Jason Roma <jasonnroma@gmail.com>
2017-10-19 00:44:29 -0400

    Typo fix and addition of DayByDayVa.org link

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-  </div><!-- end top_wrap -->
+﻿  </div><!-- end top_wrap -->
   <div id="bottom_wrap">
 	<div class="moduletable">
 	  <div id="activity_buttons">
@@ -9,11 +9,11 @@
     </div>
     <div class="moduletable">
       <div id="footer_logos">
-        <p><a target="_blank" href="http://www.lva.virginia.gov/"><img alt="Librayr of Virginia Logo" src="/img/lva_logo.png"></a><a target="_blank" href="http://www.imls.gov/"><img alt="IMLS Logo" src="/img/imls_logo.png" style="margin-right: 10px;"></a>DayByDayVA ha sido posible en parte por el Instituto de Servicios de Museos y Bibliotecas, [LS0012-0047-12 – LS00-14-0047-14].</p>
+        <p><a target="_blank" href="http://www.lva.virginia.gov/"><img alt="Library of Virginia Logo" src="/img/lva_logo.png"></a><a target="_blank" href="http://www.imls.gov/"><img alt="IMLS Logo" src="/img/imls_logo.png" style="margin-right: 10px;"></a>DayByDayVA ha sido posible en parte por el Instituto de Servicios de Museos y Bibliotecas, [LS0012-0047-12 – LS00-14-0047-14].</p>
 	  </div>
     </div>
     <div class="moduletable_footer_nav">
-      <ul class="menu"><li><a href="/about"><span>Acerca de sp.DayByDayVA.org</span></a></li><li><a href="/additional-resources"><span>Additional Resources</span></a></li><li><a href="/site-map"><span>Site Map</span></a></li></ul>
+      <ul class="menu"><li><a href="/about"><span>Acerca de sp.DayByDayVA.org</span></a></li><li><a href="/additional-resources"><span>Additional Resources</span></a></li><li><a href="/site-map"><span>Site Map</span></a></li><li><a href="http://www.daybydayva.org"><span>In english</span></a></li></ul>
     </div>
   </div><!-- end bottom_wrap -->
 </body>
